bfabeaf7529fd1fa400424b8960ad1092c05be60
jamalfazlur <fazlur.jamal@gmail.com>
2019-01-28 11:55:15 +0700

    KOMIT

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser');
 const mysql = require('mysql');
 
 var app = express();
-const port = process.env.port || 1983;
+const port = process.env.port || 1997;
 
 const conn = mysql.createConnection({
     host: 'localhost',
     user: 'jamalnew',
     password: 'bismillah114',
-    database: 'tokobuku',
+    database: 'moviebertasbih',
     port: 3306
 });
 
@@ -21,21 +21,14 @@ app.get('/', (req,res) => {
     res.send("<h1>API running! Congrats!</h1>");        
 })
 
-// ==========================================================================================================
-// ------------ Get By Query -------------------------------
-app.get('/getbuku', (req,res) => {
-    var {judul = "", kategori = "", isbn = ""} = req.query;
-    console.log(`${judul}, ${kategori}, ${isbn}`);
-    //res.send(`${judul}, ${kategori}, ${isbn}`);
+// ======================================================================================
+// ============================== MOVIES ================================================
+// ======================================================================================
 
-    var sql = `SELECT 
-    JBK.id AS 'ID Table Join', B.id AS 'ID Buku', no_isbn AS 'No ISBN', judul AS 'Judul', 
-    penulis AS 'Penulis', penerbit AS 'Penerbit', jumlah_halaman AS 'Jumlah Halaman', 
-    harga AS 'Harga', deskripsi AS 'Deskripsi', nama AS 'Kategori' 
-    FROM buku B 
-    JOIN join_buku_kategori JBK ON JBK.id_buku = B.id
-    JOIN kategori K ON JBK.id_kategori = K.id 
-    WHERE K.nama LIKE '%${kategori}%' AND judul LIKE '%${judul}%'; `;
+// ------------------- GET ALL MOVIES ---------------------------------------------------
+app.get('/getmovies', (req,res) => {
+    
+    var sql = `SELECT * FROM movies`;
 
     conn.query(sql, (err, result) => {
         if (err) throw err; 
@@ -44,97 +37,227 @@ app.get('/getbuku', (req,res) => {
 
 })
 
-// ==========================================================================================================
-// ------------ Get By Kategori (Params) -------------------
-app.get('/kategori/:keyword', (req,res) => {
-    var namaKat = req.params.keyword;
-    var sql = `SELECT 
-    JBK.id AS 'ID Table Join', 
-    B.id AS 'ID Buku', 
-    no_isbn AS 'No ISBN', 
-    judul AS 'Judul', 
-    penulis AS 'Penulis', 
-    penerbit AS 'Penerbit', 
-    jumlah_halaman AS 'Jumlah Halaman', 
-    harga AS 'Harga', 
-    deskripsi AS 'Deskripsi', 
-    nama AS 'Kategori' 
-    FROM buku B 
-    JOIN join_buku_kategori JBK ON JBK.id_buku = B.id
-    JOIN kategori K ON JBK.id_kategori = K.id 
-    WHERE K.nama LIKE '%${namaKat}%'; `;
+// ------------------- GET MOVIES BY CATEGORY (USE PARAMS) ------------------------------
+app.get('/getmovies/:keyword', (req,res) => {
+
+    var kategori = req.params.keyword;
+    var sql =  `SELECT M.id AS 'ID Movie',  
+                M.nama AS 'Nama Movie',
+                M.tahun AS 'Tahun',
+                M.description AS 'Deskripsi',
+                C.id AS 'ID Kategori',
+                C.nama AS 'Kategori'
+                FROM movcat MC
+                JOIN movies M 
+                ON MC.idmovie = M.id
+                JOIN categories C
+                ON MC.idcategory = C.id
+                WHERE C.nama LIKE '%${kategori}%';`;
 
     conn.query(sql, (err, result) => {
         if (err) throw err; 
         res.send(result);
     })
+
 })
 
-// ==========================================================================================================
+// ----------------------------- ADD MOVIES --------------------------------------------
+app.post('/addmovie', (req,res) => {
 
-app.post('/addrelation', (req,res) => {
-    var { judul, kategori } = req.body;
-    
-    var sql =   `INSERT INTO join_buku_kategori VALUES
-                (null, (SELECT id FROM buku WHERE judul LIKE '%${judul}%'), 
-                (SELECT id FROM kategori WHERE nama LIKE '%${kategori}%'));`;
-    
+    var { nama, tahun, description } = req.body;
+
+    var sql =   `INSERT INTO movies VALUES
+                (null, '${nama}', ${tahun}, '${description}');`;
+
     conn.query(sql, (err, result) => {
-        if(err) throw err;
-        res.send(`Buku ${judul} Berhasil Ditambahkan Ke Kategori ${kategori}`);
+    if(err) throw err;
+    res.send(result);
     })
 })
+// --------------------- DELETE MOVIES (USE PARAMS) ------------------------------------
+app.delete('/deletemovie/:id', (req,res) => {
+    var idMovie = req.params.id;
 
-// ==========================================================================================================
-
-app.delete('/deleterelation', (req,res) => {
-    var { judul, kategori } = req.body;
-
-    // Cek, apakah buku dengan kategori yg dimaksud ada di database untuk selanjutnya di delete?
-    var sql =  `SELECT  * FROM join_buku_kategori JBK
-                JOIN kategori K
-                ON JBK.id_kategori = K.id
-                JOIN buku B ON JBK.id_buku = B.id
-                WHERE B.judul LIKE '%${judul}%' AND K.nama LIKE '%${kategori}%';`;
+    sql = `DELETE FROM movies WHERE id = ${idMovie};`;
 
     conn.query(sql, (err, result) => {
         if(err) throw err;
-        console.log(result.length);
+        
+        sql = `DELETE FROM movcat WHERE idmovie = ${idMovie};`;
 
-        // Jika ada, maka execute query untuk delete
+        conn.query(sql, (err1, result1) => {
+            if(err1) throw err1;
+            console.log(`ID Movie: ${idMovie}, BERHASIL DIHAPUS dari MOVCAT`)
+        })
+        
+        console.log(`ID Movie: ${idMovie}, BERHASIL DIHAPUS dari MOVIES`)
+        res.send(result);
+    })
+
+})
+
+// ------------------- UPDATE MOVIES (USE PARAMS & BODY) -------------------------------
+app.post('/updatemovie/:id', (req, res) => {
+    var idMovie = req.params.id;
+    var { nama = "", tahun = "", description = "" } = req.body;
+
+    sql = `SELECT * FROM movies WHERE id = ${idMovie};`;
+
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
         if(result.length > 0){
-           var sql =   `DELETE join_buku_kategori FROM join_buku_kategori
-                        JOIN kategori ON join_buku_kategori.id_kategori = kategori.id
-                        JOIN buku ON join_buku_kategori.id_buku = buku.id
-                        WHERE (buku.judul LIKE '%${judul}%') AND (kategori.nama LIKE '%${kategori}%');`;
+            // console.log(nama, tahun, description)
+            if (nama == "") { nama = result[0].nama};
+            if (tahun == "") { tahun = result[0].tahun};
+            if (description == "") { description = result[0].description};
+
+            sql = `UPDATE movies SET 
+                    nama = '${nama}', 
+                    tahun = ${tahun}, 
+                    description = '${description}'
+                    WHERE id = ${idMovie};`;
 
             conn.query(sql, (err1, result1) => {
                 if(err1) throw err1;
-                console.log(`Buku ${judul} Berhasil Dihapus Dari Kategori ${kategori}`);
-                res.send(`Buku ${judul} Berhasil Dihapus Dari Kategori ${kategori}`);
-            }) 
-        } 
-        else {
-            res.send(`Buku yang kamu maksud tidak ditemukan`);
+                console.log('Update success');
+                res.send(result1);
+            })
         }
+        
     })
     
 })
-// ==========================================================================================================
-app.post('/updaterelation', (req,res) => {
-    var { judul, kategori, newKategori } = req.body;
 
-    var sql =   `UPDATE join_buku_kategori SET id_kategori = 
-        (SELECT id FROM kategori WHERE nama LIKE '%${newKategori}%')
-        WHERE id_buku = (SELECT id FROM buku WHERE judul LIKE '%${judul}%') 
-        AND id_kategori = ( SELECT id FROM kategori WHERE nama LIKE '%${kategori}%');`;
+// ========================================= END of API MOVIES =======================================================
+
+
+// ===================================================================================================================
+// ============================================ CATEGORIES ===========================================================
+// ===================================================================================================================
+
+// ------------------- GET ALL CATEGORIES ---------------------------------------------------
+app.get('/getcategories', (req,res) => {
     
-        conn.query(sql, (err, result) => {
-        if(err) throw err;
-        console.log(`Buku ${judul} Berhasil Dipindakan Dari Kategori ${kategori}, ke Kategori ${newKategori}`);
-        res.send(`Buku ${judul} Berhasil Dipindakan Dari Kategori ${kategori}, ke Kategori ${newKategori}`);
+    var sql = `SELECT * FROM categories`;
+
+    conn.query(sql, (err, result) => {
+        if (err) throw err; 
+        res.send(result);
+    })
+
+})
+
+// ----------------------------- ADD CATEGORY --------------------------------------------
+app.post('/addcategory', (req,res) => {
+
+    var { nama } = req.body;
+
+    var sql =   `INSERT INTO categories VALUES
+                (null, '${nama}');`;
+
+    conn.query(sql, (err, result) => {
+    if(err) throw err;
+    res.send(result);
     })
 })
+
+// --------------------- DELETE CATEGORY (USE PARAMS) -----------------------------------
+app.delete('/deletecategory/:id', (req,res) => {
+    var idCategory = req.params.id;
+
+    sql = `DELETE FROM categories WHERE id = ${idCategory};`;
+
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        
+        sql = `DELETE FROM movcat WHERE idcategory = ${idCategory};`;
+
+        conn.query(sql, (err1, result1) => {
+            if(err1) throw err1;
+            console.log(`ID Category: ${idCategory}, BERHASIL DIHAPUS dari MOVCAT`)
+        })
+        
+        console.log(`ID Category: ${idCategory}, BERHASIL DIHAPUS dari CATEGORIES`)
+        res.send(result);
+    })
+
+})
+
+// ------------------ UPDATE CATEGORY (USE PARAMS & BODY) -------------------------------
+app.post('/updatecategory/:id', (req, res) => {
+    var idCategory = req.params.id;
+    var { nama } = req.body;
+
+    sql = `SELECT * FROM categories WHERE id = ${idCategory};`;
+
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        if(result.length > 0){
+            
+            sql = `UPDATE categories SET 
+                    nama = '${nama}'
+                    WHERE id = ${idCategory};`;
+
+            conn.query(sql, (err1, result1) => {
+                if(err1) throw err1;
+                console.log('Update Category Success');
+                res.send(result1);
+            })
+        }
+        
+    })
+    
+})
+
+// ========================================= END of API CATEGORES ====================================================
+
+
+// ===================================================================================================================
+// ================================== RELATION : MOVIES <> CATEGORIES ================================================
+// ===================================================================================================================
+
+// ------------------- GET ALL RELATION ---------------------------------------------------
+app.get('/getrelation', (req,res) => {
+    
+    var sql = `SELECT M.nama AS 'Nama Movie', C.nama AS 'Kategori'
+                from movcat MC JOIN movies M 
+                ON MC.idmovie = M.id
+                JOIN categories C
+                ON MC.idcategory = C.id`;
+
+    conn.query(sql, (err, result) => {
+        if (err) throw err; 
+        res.send(result);
+    })
+
+})
+
+// ----------------------------- ADD RELATION : MOVIE <> CATEGORY --------------------------------------------
+app.post('/addrelation', (req,res) => {
+
+    var { idmovie, idcategory } = req.body;
+
+    var sql =   `INSERT INTO movcat VALUES (${idmovie}, ${idcategory});`;
+
+    conn.query(sql, (err, result) => {
+    if(err) throw err;
+    res.send(result);
+    })
+})
+
+// ------------------------------ DELETE RELATION (USE BODY) ------------------------------------------------
+app.delete('/deleterelation', (req,res) => {
+    var {idmovie, idcategory} = req.body;
+
+    sql = `DELETE FROM movcat WHERE idmovie = ${idmovie} AND idcategory = ${idcategory};`;
+
+    conn.query(sql, (err, result) => {
+        if(err) throw err;
+        res.send(result);
+    })
+
+})
+
 
 app.listen(port, () => console.log('API Aktif di port ' + port
                                     + '\nhttp://localhost:' + port))
